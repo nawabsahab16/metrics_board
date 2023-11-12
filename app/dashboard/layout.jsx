70b04a6c9f0433d02e245d4b4dd886613de08e55
nawabsahab16 <nawab162004@gmail.com>
2023-11-12 17:09:56 +0530

    feat: layout of home page with separation of diff section

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,14 +1,15 @@
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import Navbar from "../ui/dashboard/navbar/navbar"
 import { Noto_Music } from "next/font/google"
+import styles from "../ui/dashboard/dashboard.module.css";
 
 const layout = ({children}) => {
   return (
-    <div>
-        <div>
+    <div className={styles.container}>
+        <div className={styles.menu}>
             <Sidebar/>
         </div>
-        <div>
+        <div className={styles.content}>
             <Navbar />
             {children}
         </div>
---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,9 @@
+import styles from "./menuLink.module.css";
+
+const menuLink = () => {
+  return (
+    <div className={styles.container}> MenuLink </div>
+  )
+}
+
+export default menuLink
